c1dc413f193a7051caff90fdb63b498a8c542ffa
AsukaSong <yansongdong@gmail.com>
2018-09-25 10:16:40 +0800

    fix: cannot run

--- stories/index.js
+++ stories/index.js
@@ -6,23 +6,27 @@ import creatEditor from '../src/index'
 
 const UbbEditor = creatEditor()
 
+class Container extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(value) {
+    this.setState({
+      value,
+    })
+  }
+
+  render() {
+    return <UbbEditor value={this.state.value} onChange={this.onChange} />
+  }
+}
+
 storiesOf('Editor', module)
-  .add('basic use', class extends React.Component {
-    constructor(props){
-      super(props)
-      this.state = {
-        value: ''
-      }
-      this.onChange = this.onChange.bind(this)
-    }
-
-    onChange(value) {
-      this.setState({
-        value,
-      })
-    }
-
-    render() {
-      return <UbbEditor value={this.state.value} onChange={this.onChange} />
-    }
-  })
+  .add('basic use', 
+  () => <Container />
+  )
