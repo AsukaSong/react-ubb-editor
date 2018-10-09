220672b388a3bc789cb53725feecaf2fb23babc6
AsukaSong <yansongdong@gmail.com>
2018-10-09 14:04:00 +0800

    story: update welcome

--- stories/Welcome.js
+++ stories/Welcome.js
@@ -8,12 +8,46 @@ import SyntaxHighlighter from 'react-syntax-highlighter'
 import { prism } from 'react-syntax-highlighter/styles/prism';
 import 'highlight.js/styles/xcode.css'
 
+const code = `
+# @CC98/react-ubb-editor
+
+A ubb editor based on react
+
+## Install
+
+with yarn
+~~~bash
+yarn add @cc98/react-ubb-editor
+~~~
+
+or with npm
+~~~bash
+npm install --save @cc98/react-ubb-editor
+~~~
+
+## Usage
+
+~~~jsx
+import createEditor from '@cc98/react-ubb-editor'
+const Editor = createEditor()
+ReactDOM.render(<Editor />, document.getElementById('app'))
+~~~
+
+## Development
+
+~~~bash
+git clone https://github.com/AsukaSong/react-ubb-editor.git
+cd react-ubb-editor
+yarn start
+~~~
+`
+
 storiesOf('Welcome')
   .addDecorator(Layout)
   .add('readme', () => (
     <div style={{ padding: '1rem' }}>
       <ReactMarkdown
-        source={require('../README.md')}
+        source={code}
         renderers={{
           heading: ({ level, children }) => {
             const H = Head(level)
