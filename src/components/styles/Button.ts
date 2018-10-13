58ce873c82b0c9cef6fb7a7b95ace632d5931171
AsukaSong <yansongdong@gmail.com>
2018-10-13 11:11:44 +0800

    refactor: styles button

--- src/components/styles/Button.ts
+++ src/components/styles/Button.ts
@@ -1,15 +1,14 @@
 import styled from 'react-emotion'
+import center from './utils/center'
 import clear from './utils/clear'
 
 const Button = styled('button')`
   ${clear};
+  ${center};
   cursor: pointer;
   width: 1.5rem;
   height: 2rem;
   margin: 0 0.25rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 
   &:disabled {
     color: #aaa;
--- src/components/styles/ExtendRoot.ts
+++ src/components/styles/ExtendRoot.ts
@@ -1,20 +1,16 @@
-import styled, { css } from 'react-emotion'
+import styled from 'react-emotion'
 
-const ExtendRoot = styled('div')<{ isShown: boolean }>(
-  props => ({
-    height: props.isShown ? '2em' : '0em',
-  }),
-  css`
+const ExtendRoot = styled('div')<{ isShown: boolean }>`
+  height: ${props => (props.isShown ? '2em' : '0em')};
+  display: flex;
+  transition-duration: 0.1s;
+  transition-property: height;
+  padding: 0 0.5rem;
+  border-bottom: 1px solid rgb(238, 238, 238);
+
+  form {
     display: flex;
-    transition-duration: 0.1s;
-    transition-property: height;
-    padding: 0 0.5rem;
-    border-bottom: 1px solid rgb(238, 238, 238);
-
-    form {
-      display: flex;
-    }
-  `,
-)
+  }
+`
 
 export default ExtendRoot
